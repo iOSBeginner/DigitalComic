53841ae7245d0bb2d45eb5952f220ad472f0d526
蘇健豪 <jenhausu@icloud.com>
2018-07-01 01:24:15 +0800

    init project

---
+++ DigitalComic/DigitalComic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigitalComic
+//
+//  Created by 蘇健豪 on 2018/7/1.
+//  Copyright © 2018年 蘇健豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
